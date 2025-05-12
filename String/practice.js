9c04127f6015d4c08a85cefa6f30d2474fd7434b
Hanzala <hanzala.hc@gmail.com>
2025-05-12 13:02:45 +0500

    add Q8 for string

--- String/practice.js
+++ String/practice.js
@@ -276,268 +276,65 @@ const myAtoi = function (s) {
 
 */
 
-
 // Q7 Implement strStr()
 
+/*
 
+const strStr = function (haystack, needle) {
+  // handle needle 0 length case
+  if (needle.length === 0) {
+    return 0;
+  }
 
-// const strStr = function (haystack, needle) {
-//   if (needle.length === 0) {
-//     return 0;
-//   }
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack.slice(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
 
-//   for (let i = 0; i <= haystack.length - needle.length; i++) {
-//     if (haystack.slice(i, i + needle.length) === needle) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// };
+  return -1;
+};
 
-// console.log(strStr("sadbutsad", "sad"));
+*/
 
-// const longestCommonPrefix = function (strs) {
-//   if (strs.length === 0) return "";
+// Q8 Longest Common Prefix
 
-//   let prefix = strs[0];
-//   for (let i = 1; i < strs.length; i++) {
-//     let currentStr = strs[i];
-//     let tempPrefix = "";
 
-//     for (let j = 0; j < prefix.length && j < currentStr.length; j++) {
-//       if (prefix[j] === currentStr[j]) {
-//         tempPrefix += prefix[j];
-//       } else {
-//         break;
-//       }
-//     }
+/*
 
-//     prefix = tempPrefix;
+const longestCommonPrefix = function (strs) {
+  // handle 0 length case
 
-//     if (prefix === "") {
-//       return "";
-//     }
-//   }
+  if (strs.length === 0) {
+    return "";
+  }
 
-//   return prefix;
-// };
+  let prefix = strs[0];
 
-// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+  for (let i = 0; i < strs.length; i++) {
+    let tempPrefix = "";
+    let currentPrefix = strs[i];
 
-// Q1
+    for (let j = 0; j < currentPrefix.length && j < prefix.length; j++) {
+      if (currentPrefix[j] === prefix[j]) {
+        tempPrefix += currentPrefix[j];
+      } else {
+        break;
+      }
+    }
 
-// var reverseString = function (s) {
-//   if (s.length === 1) {
-//     return s;
-//   }
+    prefix = tempPrefix;
+  }
 
-//   return s.reverse();
-// };
+  // handle 0 length prefix case
 
-// console.log(reverseString(["h", "e", "l", "l", "o"]));
+  if (prefix.length === 0) {
+    return "";
+  }
 
-// Q2
+  return prefix;
+};
 
-// var reverse = function (x) {
-//   let isNegative = x < 0;
-
-//   let numToStr = Math.abs(x).toString();
-
-//   let reversedStr = numToStr.split("").reverse().join("");
-
-//   let strToInteger = parseInt(reversedStr);
-
-//   if (isNegative) {
-//     strToInteger = -strToInteger;
-//   }
-
-//   return strToInteger;
-// };
-
-// console.log(reverse(-123));
-
-// Q3
-
-// const firstUniqChar = function (stringToSolve) {
-//   let dataForStr = {};
-
-//   for (const element of stringToSolve) {
-//     if (dataForStr[element]) {
-//       dataForStr[element] = (dataForStr[element] || 0) + 1;
-//     } else {
-//       dataForStr[element] = 1;
-//     }
-//   }
-
-//   for (let i = 0; i < stringToSolve.length; i++) {
-//     if (dataForStr[stringToSolve[i]] && dataForStr[stringToSolve[i]] === 1) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// };
-
-// console.log(firstUniqChar("loveleetcode"));
-
-//Q4
-// const isAnagram = function (s, t) {
-//   if (s.length !== t.length) {
-//     return false;
-//   }
-
-//   let dataForS = new Map();
-//   let dataForT = new Map();
-
-//   for (const element of s) {
-//     dataForS.set(element, (dataForS.get(element) || 0) + 1);
-//   }
-
-//   for (const element of t) {
-//     dataForT.set(element, (dataForT.get(element) || 0) + 1);
-//   }
-
-//   for (const element of s) {
-//     if (dataForS.get(element) !== dataForT.get(element)) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// console.log(isAnagram("anagram", "nagaram"));
-
-// Q5
-// var isPalindrome = function (s) {
-//   if (s === ("" || " ")) {
-//     return true;
-//   }
-
-//   let cleanedString = s
-//     .toLowerCase()
-//     .trim()
-//     .replace(/[^a-z0-9]/gi, "");
-
-//   if (cleanedString.length === 1) {
-//     return true;
-//   }
-
-//   let reversedStr = cleanedString.split("").reverse().join("");
-
-//   if (reversedStr !== cleanedString) {
-//     return false;
-//   }
-//   return true;
-// };
-
-// console.log(isPalindrome("A man, a plan, a canal: Panama"));
-
-//Q6
-
-// const myAtoi = function (s) {
-//   if (s.length === 0) {
-//     return 0;
-//   }
-
-//   let dataForStr = [];
-//   let isNegavtive = false;
-
-//   for (const element of s) {
-//     if (element === " " && dataForStr.length === 0 && !isNegavtive) {
-//       continue;
-//     }
-
-//     if (
-//       (element === "+" || element === "-") &&
-//       !isNegavtive &&
-//       dataForStr.length === 0
-//     ) {
-//       isNegavtive = element;
-//       continue;
-//     }
-
-//     if (element === "0" && dataForStr.length === 0 && isNegavtive) {
-//       continue;
-//     }
-
-//     if (element >= "0" && element <= "9") {
-//       dataForStr.push(element);
-//     } else {
-//       break;
-//     }
-//   }
-
-//   if (dataForStr.length === 0) {
-//     return 0;
-//   }
-
-//   let strToInteger = parseInt(dataForStr.join(""));
-
-//   if (isNegavtive) {
-//     if (isNegavtive === "-") {
-//       strToInteger = -strToInteger;
-//     }
-//   }
-
-//   if (strToInteger < -(2 ** 31)) {
-//     return -(2 ** 31);
-//   }
-
-//   if (strToInteger > 2 ** 31 - 1) {
-//     return 2 ** 31 - 1;
-//   }
-
-//   return strToInteger;
-// };
-
-// console.log(myAtoi("words and 987"));
-
-// Q7
-
-// const strStr = function (haystack, needle) {
-//   if (needle.length === 0) {
-//     return 0;
-//   }
-
-//   for (let i = 0; i < haystack.length - needle.length; i++) {
-//     if (haystack.slice(i, i + needle.length) === needle) {
-//       return i;
-//     }
-//   }
-
-//   return -1;
-// };
-
-// console.log(strStr("sadbutsad", "sad"));
-
-// Q8
-
-// const longestCommonPrefix = function (strs) {
-//   if (strs.length === 0) return "";
-
-//   let prefix = strs[0];
-
-//   for (let i = 0; i < strs.length; i++) {
-//     let tempPrefix = "";
-//     let currentPrefix = strs[i];
-
-//     for (let j = 0; j < currentPrefix.length; j++) {
-//       if (currentPrefix[j] === prefix[j]) {
-//         tempPrefix += currentPrefix[j];
-//       } else {
-//         break;
-//       }
-//     }
-
-//     prefix = tempPrefix;
-//   }
-
-//   if (prefix.length === 0) {
-//     return "";
-//   }
-
-//   return prefix;
-// };
-
-// console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+*/
 
 
